555a48db0fd575838b40bdab85c244aed63cf2c9
Prottay-Ray <prottayray@gmail.com>
2021-06-12 11:36:09 +0530

    line 10 condensed

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
         $rev_num = 0;
         while($num > 1)
         {
-            $rev_num = $rev_num * 10 +
-                            $num % 10;
+            $rev_num = $rev_num * 10 + $num % 10;
             $num = (int)$num / 10;
         }
         return $rev_num;
